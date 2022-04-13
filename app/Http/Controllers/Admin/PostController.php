cae63f84dae71100b7e6aceba012fc97149d8f51
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-04-13 13:38:23 +0200

    .

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
+        //
         $posts = Post::all();
         return view('admin.post.index', compact('posts'));
     }
